4efc034e12b5118a2dbbfe11e59431e25dfe69c2
Sergio M <uo271563@uniovi.es>
2022-01-21 13:49:14 +0100

    login oauth funcional

--- cliente/servicios/viaje/buscar_viajes.php
+++ cliente/servicios/viaje/buscar_viajes.php
@@ -7,7 +7,7 @@ $destino = trim($_GET['destino']);
 $fecha = strtotime($_GET['fecha']);
 $hora = strtotime($_GET['hora']);
 
-$res = file_get_contents("http://localhost:3000/travels/search?origen=" . $origen . "&destino=" . $destino);
+$res = file_get_contents("http://pruebasergilipoopapi.herokuapp.com/travels/search?origen=" . $origen . "&destino=" . $destino);
 $data = json_decode($res);
 $viajes = array();
 
